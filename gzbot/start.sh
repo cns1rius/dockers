c5b5e5d3e391ec290bb8228d620ad53e3bdc7dfd
S1rius <cnsirius81@163.com>
2023-10-14 20:46:49 +0800

    update
    update

--- gzbot/start.sh
+++ gzbot/start.sh
@@ -9,7 +9,7 @@ sleep 3
 cd gzbot
 
 echo qqbot python
-python3 gzbot.py --url="http://10.101.162.163" --notice=$1 --id=$2 --port=5700 &
+python3 gzbot.py --url="$1" --notice=$2 --id=$3 --port=5700 &
 
 echo qq server
 go-cqhttp
